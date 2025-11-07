4c9d9432118c002017110c3fb56ec016f06eda62
AlexT <xp___alex@mail.ru>
2025-11-07 14:40:29 +0300

    feat: add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import Header from './components/Header';
 import Categories from './components/categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from '../src/assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [fetchPizzas, setFetchPizzas] = useState([]);
+
+  useEffect(() => {
+    async function getPizzas() {
+      const response = await fetch('https://62aa24433b3143855442d35b.mockapi.io/items')
+        .then((response) => response.json())
+        .then((data) => setFetchPizzas(data));
+    }
+    getPizzas();
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -15,9 +26,12 @@ function App() {
             <Categories />
             <Sort />
           </div>
+          <div>
+            <h1>Это лизин сайт</h1>
+          </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
+            {fetchPizzas.map((pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+
 export default function Sort() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const list = ['популярности', 'цене', 'алфавиту'];
+  const sortName = list[selected];
+  function handleClick() {
+    setIsOpen((prev) => !prev);
+  }
+
+  function handleSort(item) {
+    setSelected(item);
+    setIsOpen(false);
+  }
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -14,15 +29,25 @@ export default function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={handleClick}>{sortName}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((data, index) => (
+              <li
+                onClick={() => handleSort(index)}
+                className={selected === index ? 'active' : ''}
+                key={index}>
+                {data}
+              </li>
+            ))}
+            {/* <li className="active">популярности</li>
+            <li>цене</li>
+            <li>алфавиту</li> */}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
